62914bb219479ac53326971978649112a18cf6fc
Sakhil Sharma <sakhilsharma123@gmail.com>
2024-07-04 00:02:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,11 @@ function stopQuotes(){
 }
 
 quote_btn.addEventListener("click",()=>{
+    confetti({
+  particleCount: 100,
+  spread: 70,
+  origin: { y: 0.6 },
+});
     count++;
 if(count <= 5){
     RandomQuoteGenerator();
